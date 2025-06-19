9c27b2d022750f12284b172e9d6270234789980f
Nathan Walsh <nwalsh03@icloud.com>
2025-06-19 16:22:01 +0100

    chore: add Firebase polyfills and init service module

--- App.tsx
+++ App.tsx
@@ -1,3 +1,6 @@
+import 'react-native-get-random-values';
+import 'react-native-url-polyfill/auto';
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
